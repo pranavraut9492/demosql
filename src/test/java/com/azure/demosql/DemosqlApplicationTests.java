3afa2433d1ef838b1310417f4bb99ea0b1fc8574
Pranav Raut <163521474+pranavraut9492@users.noreply.github.com>
2024-04-16 20:30:21 +0100

    Update DemosqlApplicationTests.java

--- src/test/java/com/azure/demosql/DemosqlApplicationTests.java
+++ src/test/java/com/azure/demosql/DemosqlApplicationTests.java
@@ -1,13 +1,1 @@
-package com.azure.demosql;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemosqlApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
